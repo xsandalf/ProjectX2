15ca119a270d27b7e22aba5914cd3884851157f5
xsandalf <44393930+xsandalf@users.noreply.github.com>
2023-09-19 13:31:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,12 @@ int main() {
     // Function calls to calculate
     // Write your all possible input scenarios here
     // Notice that calculator code written here works with floats now
+    calculator(3.0f, "+", 76.2f);
+    calculator(2.4f, "-", 3445.0f);
+    calculator(13.3333f, "*", 27.3452f);
+    calculator(0.278931f, "/", 9.0f);
+    calculator(1.0f, "/", 0.0f);
+    calculator(0.0f, "a", 0.01f);
+    
     return 0;
 }
